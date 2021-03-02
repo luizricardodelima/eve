84974178f6a7e9ccdf6612316a580d7b82b108dc
Ricardo Lima <luizricardodelima@hotmail.com>
2021-03-02 00:16:00 -0300

    Better page formatting (borders, resizable imgs)

---
+++ bootstraptest.php
@@ -0,0 +1,258 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+<meta http-equiv="Content-Type" content="text/html;charset=utf-8"/>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css" rel="stylesheet">
+<title>EVE - Eventos</title>
+</head>
+<body class="bg-light">
+
+
+<nav class="navbar navbar-expand-lg fixed-top navbar-dark bg-dark" aria-label="Main navigation">
+<div class="container-fluid">
+<a class="navbar-brand" href="#">Offcanvas navbar</a>
+<button class="navbar-toggler p-0 border-0" type="button" data-bs-toggle="offcanvas" aria-label="Toggle navigation">
+<span class="navbar-toggler-icon"></span>
+</button>
+
+<div class="navbar-collapse offcanvas-collapse" id="navbarsExampleDefault">
+<ul class="navbar-nav me-auto mb-2 mb-lg-0">
+<li class="nav-item active">
+<a class="nav-link" aria-current="page" href="#">Dashboard</a>
+</li>
+<li class="nav-item">
+<a class="nav-link" href="#">Notifications</a>
+</li>
+<li class="nav-item">
+<a class="nav-link" href="#">Profile</a>
+</li>
+<li class="nav-item">
+<a class="nav-link" href="#">Switch account</a>
+</li>
+<li class="nav-item dropdown">
+<a class="nav-link dropdown-toggle" href="#" id="dropdown01" data-bs-toggle="dropdown" aria-expanded="false">Settings</a>
+<ul class="dropdown-menu" aria-labelledby="dropdown01">
+<li><a class="dropdown-item" href="#">Action</a></li>
+<li><a class="dropdown-item" href="#">Another action</a></li>
+<li><a class="dropdown-item" href="#">Something else here</a></li>
+</ul>
+</li>
+</ul>
+<form class="d-flex">
+<input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+<button class="btn btn-outline-success" type="submit">Search</button>
+</form>
+</div>
+</div>
+</nav>
+
+<div class="nav-scroller bg-white shadow-sm">
+<nav class="nav nav-underline" aria-label="Secondary navigation">
+<a class="nav-link active" aria-current="page" href="#">Dashboard</a>
+<a class="nav-link" href="#">
+Friends
+<span class="badge bg-light text-dark rounded-pill align-text-bottom">27</span>
+</a>
+<a class="nav-link" href="#">Explore</a>
+<a class="nav-link" href="#">Suggestions</a>
+<a class="nav-link" href="#">Link</a>
+<a class="nav-link" href="#">Link</a>
+<a class="nav-link" href="#">Link</a>
+<a class="nav-link" href="#">Link</a>
+<a class="nav-link" href="#">Link</a>
+</nav>
+</div>
+
+<main class="container">
+
+<div class="accordion" id="accordionExample">
+<div class="accordion-item">
+<h2 class="accordion-header" id="headingOne">
+<button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+Opções do usuário
+</button>
+</h2>
+<div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+<div class="accordion-body container">
+<div class="row row-cols-auto justify-content-start gx-2 gy-2">
+
+<div class="col">
+<button type="button" class="btn btn-primary" style="height: 8em; width: 8em;">
+<i class="bi-person d-block display-5"></i>
+Cadastro
+</button>
+</div>
+
+<div class="col">
+<button type="button" class="btn btn-primary" style="height: 8em; width: 8em;">
+<i class="bi-person-check d-block display-5"></i>
+Cadastro
+</button>
+</div>
+
+<div class="col">
+<button type="button" class="btn btn-primary" style="height: 8em; width: 8em;">
+<i class="bi-cart4 d-block display-5"></i>
+Pagamento
+</button>
+</div>
+
+<div class="col">
+<button type="button" class="btn btn-primary" style="height: 8em; width: 8em;">
+<i class="bi-file-earmark-text d-block display-5"></i>
+Trabalho
+</button>
+</div>
+
+<div class="col">
+<button type="button" class="btn btn-primary" style="height: 8em; width: 8em;">
+<i class="bi-award d-block display-5"></i>
+Certificado
+</button>
+</div>
+
+<div class="col">
+<button type="button" class="btn btn-outline-primary" style="height: 8em; width: 8em;">
+<i class="bi-lock d-block display-5"></i>
+Alterar senha
+</button>
+</div>
+
+<div class="col">
+<button type="button" class="btn btn-outline-primary" style="height: 8em; width: 8em;">
+<i class="bi-door-open d-block display-5"></i>
+Sair
+</button>
+</div>
+
+</div>
+</div>
+</div>
+</div>
+<div class="accordion-item">
+<h2 class="accordion-header" id="headingTwo">
+<button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+Opções do revisor
+</button>
+</h2>
+<div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+<div class="accordion-body container">
+
+<div class="row row-cols-auto justify-content-start gx-2 gy-2">
+
+<div class="col">   
+<button type="button" class="btn btn-primary" style="height: 8em; width: 8em;">
+<i class="bi-file-earmark-check d-block display-5"></i>
+Trabalho
+</button>
+</div>
+
+<div class="col">
+<button type="button" class="btn btn-primary" style="height: 8em; width: 8em;">
+<i class="bi-file-earmark-check d-block display-5"></i>
+Trabalho
+</button>
+</div>
+
+</div>
+
+</div>
+</div>
+</div>
+<div class="accordion-item">
+<h2 class="accordion-header" id="headingThree">
+<button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+Opções do administrador
+</button>
+</h2>
+<div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
+<div class="accordion-body container">
+
+<div class="row row-cols-auto justify-content-start gx-2 gy-2">
+
+<div class="col">
+<button type="button" class="btn btn-primary" style="height: 8em; width: 8em;">
+<i class="bi-person-x d-block display-5"></i>
+Usuários sem verificação
+</button>
+</div>
+
+<div class="col">
+<button type="button" class="btn btn-primary" style="height: 8em; width: 8em;">
+<i class="bi-people d-block display-5"></i>
+Usuários
+</button>
+</div>
+
+<div class="col">
+<button type="button" class="btn btn-primary" style="height: 8em; width: 8em;">
+<i class="bi-file-earmark d-block display-5"></i>
+Definições de submissão
+</button>
+</div>
+
+<div class="col">
+<button type="button" class="btn btn-primary" style="height: 8em; width: 8em;">
+<i class="bi-cart4 d-block display-5"></i>
+Pagamentos
+</button>
+</div>
+
+<div class="col">
+<button type="button" class="btn btn-primary" style="height: 8em; width: 8em;">
+<i class="bi-cart4 d-block display-5"></i>
+Opções de pagamento
+</button>
+</div>
+
+<div class="col">
+<button type="button" class="btn btn-primary " style="height: 8em; width: 8em;">
+<i class="bi-cart4 d-block display-5"></i>
+Grupos de pagamento
+</button>
+</div>
+
+<div class="col">
+<button type="button" class="btn btn-primary" style="height: 8em; width: 8em;">
+<i class="bi-award d-block display-5"></i>
+Certificados
+</button>
+</div>
+
+<div class="col">
+<button type="button" class="btn btn-primary" style="height: 8em; width: 8em;">
+<i class="bi-award-fill d-block display-5"></i>
+Modelos de certificado
+</button>
+</div>
+
+<div class="col">
+<button type="button" class="btn btn-primary" style="height: 8em; width: 8em;">
+<i class="bi-window d-block display-5"></i>
+Páginas
+</button>
+</div>
+
+<div class="col">
+<button type="button" class="btn btn-primary" style="height: 8em; width: 8em;">
+<i class="bi-gear d-block display-5"></i>
+Ajustes do sistema
+</button>
+</div>
+
+</div>
+</div>
+</div>
+</div>
+</div>
+
+
+</main>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+</body>
+</html>
